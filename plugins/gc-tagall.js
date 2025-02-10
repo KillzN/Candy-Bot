83c0777e0a8295c73ebe228ac0410ce521ca72bb
KillzN <scorpionsheep203@gmail.com>
2025-02-09 21:43:54 -0300

    KILL BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,10 +5,10 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
   }
   let pesan = args.join` `
   let oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`
-  let teks = `*> 𝗔𝗰𝘁𝗶𝘃𝗲𝗻𝘀𝗲 𝗽𝗹𝗮𝗻𝘁𝗮𝘀 𝗲𝘀 𝗵𝗼𝗿𝗮 𝗱𝗲 𝘁𝗲𝗻𝗲𝗿 𝘂𝗻 𝗯𝗼𝗻𝗶𝘁𝗼 𝗱𝗶́𝗮*\n\n ${oi}\n\n➥ _*@the_candy_ff:*_\n`
+  let teks = `*𝗡𝗨𝗘𝗩𝗢 𝗜𝗡𝗧𝗘𝗚𝗥𝗔𝗡𝗧𝗘 , 𝗤𝗨𝗘 𝗘𝗦𝗣𝗘𝗥𝗔𝗡 𝗣𝗔𝗥𝗔 𝗗𝗔𝗥 𝗟𝗔 𝗕𝗜𝗘𝗡𝗩𝗘𝗡𝗜𝗗𝗔, 𝗟𝗘𝗦 𝗥𝗨𝗘𝗚𝗢 𝗢 𝗤𝗨𝗘 ❓*\n\n ${oi}\n\n➥ _*@tribe_pride*_\n`
   for (let mem of participants) {
-  teks += `🍭⇝ @${mem.id.split('@')[0]}\n`}
-  teks += `└ *Cᴀɴᴅʏ-Bᴏᴛ ⇝ @the_candy_ff*`
+  teks += `🪂 ⇝ @${mem.id.split('@')[0]}\n`}
+  teks += `*└Bot - TRϟBEㅤPRϟDE*`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
   handler.help = ['tagall <mesaje>','invocar <mesaje>']
